5567a0360c30c6437c8bc6717936f98a65525f56
PaulKorDev <pauldeveloper@outlook.com>
2024-07-06 02:36:09 -0700

    Create loading screen

--- Assets/Scripts/UnitFactory/PrefabPaths.cs
+++ Assets/Scripts/UnitFactory/PrefabPaths.cs
@@ -6,6 +6,6 @@
     public const string UNIT_3 = "Units/Prefabs/Unit_3";
 
     //UI
-    //public const string LOADING_SCREEN = "";
+    public const string LOADING_SCREEN = "ServiceObjects/LoadingScreen";
 }
 

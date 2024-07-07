052645becc3d729c94ab746df6a1fbcac1551699
PaulKorDev <pauldeveloper@outlook.com>
2024-07-07 10:51:32 -0700

    Add very simple EventBus
    Perhaps I will create a more complex EventBus later.

---
+++ Assets/Scripts/Architecture/EventBus/EventBus.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Events;
+
+namespace Assets.Scripts.Architecture.EventBus
+{
+
+    public class EventBus
+    {
+        public UnityEvent UnitsOrderChanged { get; } = new();
+
+        public void TrigerUnitsOrderChanged() => UnitsOrderChanged?.Invoke();
+
+    }
+}

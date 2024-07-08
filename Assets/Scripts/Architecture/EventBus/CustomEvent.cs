54d74987ebc96006904436a5c8372eec94c05be9
PaulKorDev <pauldeveloper@outlook.com>
2024-07-08 16:21:06 -0700

    Completed the Custom Event.  Calling listeners by priority works. I am very proud of it.
    1/ Refactored ListenerWithPriority
    2/ Add validate for Subscribe and Unsubcribe in Dictionaty

--- Assets/Scripts/Architecture/EventBus/CustomEvent.cs
+++ Assets/Scripts/Architecture/EventBus/CustomEvent.cs
@@ -1,26 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assets.Scripts.Architecture.EventBus
 {
-    public class CustomEvent
+    public class CustomEvent : BaseCustomEvent<Action>
     {
-        protected Dictionary<Action, ListenerWithPriority> _listeners = new();
-
-        public void Subscribe(Action listener, int priority = 0)
-        {
-            Action key = listener;
-            _listeners.Add(key, new ListenerWithPriority(listener, priority));
-        }
-        public void Unsubscribe(Action listener)
-        {
-            Action key = listener;
-            _listeners.Remove(key);
-        }
         public void Trigger()
         {
-            foreach (var listener in _listeners) {
-                listener.Value.Listener.Invoke();
+            var tempListeners = _listeners.Values.ToList();
+
+            foreach (var listener in tempListeners) 
+                listener.Listener.Invoke();
+        }
+    }
+    public class CustomEvent<T1> : BaseCustomEvent<Action<T1>>
+    {
+        public void Trigger(T1 arg)
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.Value.Listener.Invoke(arg);
+            }
+        }
+    }
+    public class CustomEvent<T1, T2> : BaseCustomEvent<Action<T1, T2>>
+    {
+        public void Trigger(T1 arg, T2 arg2)
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.Value.Listener.Invoke(arg, arg2);
+            }
+        }
+    }
+    public class CustomEvent<T1, T2, T3> : BaseCustomEvent<Action<T1, T2, T3>>
+    {
+        public void Trigger(T1 arg, T2 arg2, T3 arg3)
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.Value.Listener.Invoke(arg, arg2, arg3);
+            }
+        }
+    }
+    public class CustomEvent<T1, T2, T3, T4> : BaseCustomEvent<Action<T1, T2, T3, T4>>
+    {
+        public void Trigger(T1 arg, T2 arg2, T3 arg3, T4 arg4)
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.Value.Listener.Invoke(arg, arg2, arg3, arg4);
             }
         }
     }
--- Assets/Scripts/Architecture/EventBus/ListenerWithPriority.cs
+++ Assets/Scripts/Architecture/EventBus/ListenerWithPriority.cs
@@ -2,57 +2,19 @@
 
 namespace Assets.Scripts.Architecture.EventBus
 {
-    //Here is many repeats but I hope it's not too critical
-    public abstract class BaseListenerWithPriority<T>
+    /// <summary>
+    /// Used descending order (higher priority first)
+    /// </summary>
+    public class ListenerWithPriority<ACTION>
     {
-        public readonly T Listener;
+        public readonly ACTION Listener;
         public readonly int Priority;
 
-        protected BaseListenerWithPriority(T listener, int priority)
+        public ListenerWithPriority(ACTION listener, int priority)
         {
             Listener = listener;
             Priority = priority;
         }
     }
-    public class ListenerWithPriority : BaseListenerWithPriority<Action>
-    {
-        public ListenerWithPriority(Action listener, int priority) : base(listener, priority){}
-    }
-    public class ListenerWithPriority<T1> : BaseListenerWithPriority<Action<T1>>
-    {
-        public ListenerWithPriority(Action<T1> listener, int priority) : base(listener, priority){}
-    }
-    public class ListenerWithPriority<T1, T2> 
-    {
-        public readonly Action<T1, T2> Listener;
-        public readonly int Priority;
-
-        public ListenerWithPriority(Action<T1, T2> listener, int priority)
-        {
-            Listener = listener;
-            Priority = priority;
-        }
-    }
-    public class ListenerWithPriority<T1,T2,T3>
-    {
-        public readonly Action<T1, T2, T3> Listener;
-        public readonly int Priority;
-
-        public ListenerWithPriority(Action<T1, T2, T3> listener, int priority)
-        {
-            Listener = listener;
-            Priority = priority;
-        }
-    }
-    public class ListenerWithPriority<T1, T2, T3,T4>
-    {
-        public readonly Action<T1, T2, T3, T4> Listener;
-        public readonly int Priority;
-
-        public ListenerWithPriority(Action<T1, T2, T3, T4> listener, int priority)
-        {
-            Listener = listener;
-            Priority = priority;
-        }
-    }
+   
 }

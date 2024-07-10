974268bd8d639db8f77990a18440363b06b64f32
PaulKorDev <pauldeveloper@outlook.com>
2024-07-10 06:53:33 -0700

    merge conflict

--- Assets/Scripts/Architecture/Reactive/ReactiveProperty.cs
+++ Assets/Scripts/Architecture/Reactive/ReactiveProperty.cs
@@ -4,20 +4,11 @@ namespace Assets.Scripts.Architecture.Reactive
 {
     public class ReactiveProperty<T>
     {
-<<<<<<< Updated upstream:Assets/Scripts/Architecture/ReactiveProperty/ReactiveProperty.cs
-        public ReactiveProperty(T defaultValue)
-        {
-            Value = defaultValue;
-        }
+
         public event Action<T> OnChanged;
 
         private T _value;
 
-=======
-        public event Action<T> OnChanged;
-
-        private T _value;
->>>>>>> Stashed changes:Assets/Scripts/Architecture/Reactive/ReactiveProperty.cs
         public T Value
         {
             get { return _value; }
